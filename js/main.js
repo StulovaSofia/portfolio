96bdc37521cbe5d79e6ea5a1f8215064cd44a15d
Sonya2114 <stulova.2004@mail.ru>
2022-10-13 22:40:57 +0700

    1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// const nav = document.querySelector('#nav');
+// const navBtn = document.querySelector('#nav-btn');
+// const navBtnImg = document.querySelector('#nav-btn-img');
+
+// navBtn.onclick = () => {
+// 	if (nav.classList.toggle('nav__open')) {
+// 		navBtnImg.src = './images/nav-close.svg';
+// 	} else {
+// 		navBtnImg.src = './images/nav.svg';
+// 	}
+// }
+
+AOS.init(
+	{
+		// once: true
+	}
+);
